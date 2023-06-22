6f12ddc4411b8d4a4c4b582d0f30d8b124c28a89
alinesaei <nesaeialii@gmail.com>
2023-06-22 17:05:25 +0330

    Updated the style of Home page

--- gui.py
+++ gui.py
@@ -58,58 +58,66 @@ def Home():
     
     #ABOUT POWERPOINT GENERATOR
     with st.container():
-        image_container, text_container = st.columns((1, 2))
-        with image_container:
-            # gpt_logo = Image.open('images/gptlogo.png', mode='r')
-            # st.image(gpt_logo, use_column_width='always')
-            with tempfile.TemporaryDirectory() as temp_dir:
-                gpt_logo_path = os.path.join(temp_dir, "gptlogo.png")
-                gpt_logo = Image.open('logo/gptlogo.png', mode='r')
-                gpt_logo.save(gpt_logo_path)
-                st.image(gpt_logo_path, use_column_width='always')
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            image_container, text_container = st.columns((1, 2))
+            with image_container:
+                # gpt_logo = Image.open('images/gptlogo.png', mode='r')
+                # st.image(gpt_logo, use_column_width='always')
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    gpt_logo_path = os.path.join(temp_dir, "gptlogo.png")
+                    gpt_logo = Image.open('logo/gptlogo.png', mode='r')
+                    gpt_logo.save(gpt_logo_path)
+                    st.image(gpt_logo_path, use_column_width='always')
 
             
-        with text_container:
-           st.markdown(
-                """
-                The PowerPoint Generator leverages the power of GPT-3.5, an advanced language model developed by OpenAI. By harnessing the capabilities of GPT-3.5, it utilizes artificial intelligence to rapidly generate detailed content for a wide range of topics. This ensures that the generated slides contain accurate and relevant information, saving you time and effort in researching and compiling content for your presentation.
-                """
-            )
+            with text_container:
+                st.markdown(
+                        """
+                        The PowerPoint Generator leverages the power of GPT-3.5, an advanced language model developed by OpenAI. By harnessing the capabilities of GPT-3.5, it utilizes artificial intelligence to rapidly generate detailed content for a wide range of topics. This ensures that the generated slides contain accurate and relevant information, saving you time and effort in researching and compiling content for your presentation.
+                        """
+                    )
            
-    st.divider()
-    #ABOUT SUMMARIZER
-    with st.container():
-        image_container, text_container = st.columns((1, 2))
-        with image_container:
-            # summarizer_logo = Image.open('images/summarizer.jpg', mode='r')
-            # st.image(summarizer_logo, use_column_width='always')
-            with tempfile.TemporaryDirectory() as temp_dir:
-                summarizer_logo_path = os.path.join(temp_dir, "summarizer.jpg")
-                summarizer_logo = Image.open('logo/summarizer.jpg', mode='r')
-                summarizer_logo.save(summarizer_logo_path)
-                st.image(summarizer_logo_path, use_column_width='always')
 
-        with text_container:
-            st.markdown(
-                """
-                The Summarizer is a powerful text summarization tool that utilizes advanced natural language processing techniques to generate concise summaries of large blocks of text. It helps users extract key information and main points from lengthy documents, articles, or any text input.
-                Using state-of-the-art language models, the Summarizer analyzes the input text, identifies important sentences or passages, and generates a condensed summary that captures the essence of the original content. The summarization process saves time and allows users to quickly grasp the main ideas without having to read through the entire text.
-                With the Summarizer, you can effectively summarize research papers, news articles, blog posts, or any textual content, enabling you to digest information more efficiently and make informed decisions. Simply input your text, click the "Summarize" button, and let the Summarizer do the rest!
-                """
-            )
+    #ABOUT SUMMARIZER
+    with col2:
+            image_container, text_container = st.columns((1, 2))
+            with image_container:
+                # summarizer_logo = Image.open('images/summarizer.jpg', mode='r')
+                # st.image(summarizer_logo, use_column_width='always')
+                with tempfile.TemporaryDirectory() as temp_dir:
+                    summarizer_logo_path = os.path.join(temp_dir, "summarizer.jpg")
+                    summarizer_logo = Image.open('logo/summarizer.jpg', mode='r')
+                    summarizer_logo.save(summarizer_logo_path)
+                    st.image(summarizer_logo_path, use_column_width='always')
+
+            with text_container:
+                st.markdown(
+                    """
+                    The Summarizer is a powerful text summarization tool that utilizes advanced natural language processing techniques to generate concise summaries of large blocks of text. It helps users extract key information and main points from lengthy documents, articles, or any text input.
+                    Using state-of-the-art language models, the Summarizer analyzes the input text, identifies important sentences or passages, and generates a condensed summary that captures the essence of the original content. The summarization process saves time and allows users to quickly grasp the main ideas without having to read through the entire text.
+                    With the Summarizer, you can effectively summarize research papers, news articles, blog posts, or any textual content, enabling you to digest information more efficiently and make informed decisions. Simply input your text, click the "Summarize" button, and let the Summarizer do the rest!
+                    """
+                )
         
  
 def generate_ppt():
     st.write("Enter comma-separated topics:")
     input_text = st.text_input("Topics")
-    difficulty = st.select_slider(
-        'Select the diffulcty of the content',
-        options=['easy', 'medium', 'hard']
-    )
-    keyword = st.text_input('Keywords')
     with st.container():
-        language_container, color_container = st.columns((1, 1))
+        difficulty_container, color_container = st.columns((3, 1))
+        with difficulty_container:
+            difficulty = st.select_slider(
+                'Select the diffulcty of the content',
+                options=['easy', 'medium', 'hard']
+            )
+        with color_container:
+                color = st.color_picker('Pick A Color for the background', '#00f900').lstrip('#')
         
+    with st.container():
+        keyword_container, language_container = st.columns((1, 1))
+        with keyword_container:
+            keyword = st.text_input('Keywords')
         with language_container:
             language = st.selectbox(
             'Select the language',
@@ -117,8 +125,7 @@ def generate_ppt():
 
             if language == 'فارسی':
                 language = 'farsi'
-        with color_container:
-            color = st.color_picker('Pick A Color for the background', '#00f900').lstrip('#')
+        
     file_name_save = st.text_input('file name')
     with st.container():
         author_container, presentation_title_container= st.columns((1, 1))
